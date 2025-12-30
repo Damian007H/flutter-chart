86f017ef85239d26ff58cba164cb2a9ff857b0b9
Damian <damian@123.com>
2025-12-30 12:05:59 +0800

    禁止左右滑动，将图表据中显示

--- test_project/lib/widgets/kline_widget.dart
+++ test_project/lib/widgets/kline_widget.dart
@@ -4,40 +4,81 @@ import 'package:flutter/material.dart';
 
 import 'kline_theme.dart';
 
-class KChart extends StatelessWidget {
+class KChart extends StatefulWidget {
   final List<Tick> ticks;
 
-  KChart(this.ticks, {super.key});
+  const KChart(this.ticks, {super.key});
 
+  @override
+  State<KChart> createState() => _KChartState();
+}
+
+class _KChartState extends State<KChart> {
   final ChartController _controller = ChartController();
+
+  @override
+  void initState() {
+    super.initState();
+    _lockHorizontalScroll();
+  }
+
+  @override
+  void didUpdateWidget(covariant KChart oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    _lockHorizontalScroll();
+  }
+
+  void _lockHorizontalScroll() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _controller.toggleXScrollBlock?.call(isXScrollBlocked: true);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     bool isLight = true;
     ChartDefaultTheme theme = isLight ? KLineLight() : KLineLight();
-    return DerivChart(
-      mainSeries: LineSeries(ticks),
-      activeSymbol: "BTC",
-      granularity: 1000,
-      theme: theme,
-      indicatorsRepo: AddOnsRepository<IndicatorConfig>(
-        createAddOn: (m) => IndicatorConfig.fromJson(m),
-        onEditCallback: (_) {},
-        sharedPrefKey: 'BTC',
-      ),
-      drawingToolsRepo: AddOnsRepository<DrawingToolConfig>(
-        createAddOn: (m) => DrawingToolConfig.fromJson(m),
-        onEditCallback: (_) {},
-        sharedPrefKey: 'BTC',
-      ),
-      controller: _controller,
-      // ...
-      chartAxisConfig: const ChartAxisConfig(
-        yAxisLabelPosition: YAxisLabelPosition.left,
-        yAxisLabelsOverlay: true,
-      ),
-      showCrosshair: false,
-      isLive: true,
+
+    final int targetVisiblePoints = widget.ticks.length;
+    const int granularityMs = 5 * 60 * 1000;
+
+    return LayoutBuilder(
+      builder: (BuildContext context, BoxConstraints constraints) {
+        final double chartWidthPx = constraints.maxWidth;
+        final double intervalWidthPx = (chartWidthPx / targetVisiblePoints).clamp(1.0, 80.0);
+        final double msPerPx = granularityMs / intervalWidthPx;
+        final double dataWidthPx = intervalWidthPx * widget.ticks.length;
+        final double centerPaddingPx = ((chartWidthPx - dataWidthPx) / 2).clamp(0.0, chartWidthPx);
+
+        return DerivChart(
+          mainSeries: LineSeries(widget.ticks),
+          activeSymbol: "BTC",
+          granularity: granularityMs,
+          theme: theme,
+          indicatorsRepo: AddOnsRepository<IndicatorConfig>(
+            createAddOn: (m) => IndicatorConfig.fromJson(m),
+            onEditCallback: (_) {},
+            sharedPrefKey: 'BTC',
+          ),
+          drawingToolsRepo: AddOnsRepository<DrawingToolConfig>(
+            createAddOn: (m) => DrawingToolConfig.fromJson(m),
+            onEditCallback: (_) {},
+            sharedPrefKey: 'BTC',
+          ),
+          controller: _controller,
+          msPerPx: msPerPx,
+          minIntervalWidth: intervalWidthPx,
+
+          maxIntervalWidth: intervalWidthPx,
+          chartAxisConfig: ChartAxisConfig(
+            yAxisLabelPosition: YAxisLabelPosition.left,
+            yAxisLabelsOverlay: true,
+            maxCurrentTickOffset: centerPaddingPx,
+          ),
+          showCrosshair: false,
+          isLive: true,
+        );
+      },
     );
   }
 }
